999403ee55900ab30483ed4be3ced488f36be199
AyushSharma3665 <ayush8126366594@gmail.com>
2026-02-20 17:15:30 +0530

    add UI componenent in the home screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,9 +8,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final List<Map<String, dynamic>> menuItems = [
+    {'title': 'Home', 'icon': Icons.home},
+    {'title': 'Profile', 'icon': Icons.person},
+    {'title': 'Settings', 'icon': Icons.settings},
+    {'title': 'Logout', 'icon': Icons.logout},
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      endDrawer: Drawer(
+        backgroundColor: Colors.black,
+        child: Column(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.grey[900]),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "Ayush Sharma",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: menuItems.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Icon(
+                      menuItems[index]['icon'],
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      menuItems[index]['title'],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.black,
       body: Stack(
         children: [
@@ -22,16 +71,214 @@ class _HomePageState extends State<HomePage> {
           ),
           SafeArea(
             child: SingleChildScrollView(
-              padding: EdgeInsets.all(12),
+              padding: EdgeInsets.all(16),
               child: Column(
+                spacing: 16,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('UserName', style: TextStyle(color: Colors.white)),
+                      Text(
+                        'Ayush Sharma',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
                       IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          _scaffoldKey.currentState?.openEndDrawer();
+                        },
                         icon: Icon(Icons.menu, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Ayush',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 34,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 60.0),
+                        child: Text(
+                          'Sharma',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 34,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'My goal is to write maintainable, clean, and understandable code so that the development process is enjoyable',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+
+                  Row(
+                    spacing: 16,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          maximumSize: Size(250, 50),
+                          minimumSize: Size(150, 40),
+                        ),
+                        child: Text(
+                          'Projects',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          maximumSize: Size(60, 50),
+                          minimumSize: Size(40, 40),
+                        ),
+                        child: Icon(Icons.arrow_forward, color: Colors.black),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    spacing: 16,
+                    children: [
+                      OutlinedButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.link, color: Colors.white),
+                        style: ElevatedButton.styleFrom(
+                          maximumSize: Size(250, 50),
+                          minimumSize: Size(150, 40),
+                        ),
+                        label: Text(
+                          'LinkedIn',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      OutlinedButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.code, color: Colors.white),
+                        style: ElevatedButton.styleFrom(
+                          maximumSize: Size(250, 50),
+                          minimumSize: Size(150, 40),
+                        ),
+                        label: Text(
+                          'GitHub',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      // Base Container
+                      Container(
+                        height: 300,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(34),
+                        ),
+                      ),
+
+                      // Overlay Container
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          color: Colors.black.withValues(alpha: 0.3),
+                        ),
+                        alignment: Alignment.topCenter,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Saarthi GPS",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8,
+                              ),
+                              child: Text(
+                                "Saarthi GPS is a real-time CNG Locator App offering live location monitoring, route playback and centralized fleet management through a modern dashboard.",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                            Row(
+                              spacing: 16,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    maximumSize: Size(150, 40),
+                                    minimumSize: Size(60, 30),
+                                  ),
+                                  child: Text(
+                                    'Read More',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 16,
+                                      fontStyle: FontStyle.italic,
+                                    ),
+                                  ),
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    maximumSize: Size(60, 40),
+                                    minimumSize: Size(40, 30),
+                                  ),
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
